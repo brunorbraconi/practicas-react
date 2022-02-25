784070f503bcf6b5a8c8e73e9c37af98143f68aa
Bruno Braconi <brunorbraconi@hotmail.com>
2022-02-24 23:36:44 -0300

    creacion de tarjetas con props

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,66 +1,41 @@
 import React from "react";
+import Cards from "./Cards";
 
-function ContentRowMovies(){
-    return (
-<React.Fragment>
-{/* <!-- Content Row Movies--> */}
-<div className="row">
+let movies = {
+  title: "Movies in Data Base",
+  color: "primary",
+  value: 21,
+  icon: "film",
+};
+let awards = {
+  title: "Total awards",
+  color: "success",
+  value: 79,
+  icon: "award",
+};
 
-{/* <!-- Movies in Data Base --> */}
-<div className="col-md-4 mb-4">
-    <div className="card border-left-primary shadow h-100 py-2">
-        <div className="card-body">
-            <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                    <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-                    <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-                </div>
-                <div className="col-auto">
-                    <i className="fas fa-film fa-2x text-gray-300"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+let actors = {
+  title: "Actors quantity",
+  color: "warning",
+  value: 49,
+  icon: "user",
+};
 
-{/* <!-- Total awards --> */}
-<div className="col-md-4 mb-4">
-    <div className="card border-left-success shadow h-100 py-2">
-        <div className="card-body">
-            <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                    <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                    <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                </div>
-                <div className="col-auto">
-                    <i className="fas fa-award fa-2x text-gray-300"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+let cards = [movies, awards, actors];
+// console.log(cards)
 
-{/* <!-- Actors quantity --> */}
-<div className="col-md-4 mb-4">
-    <div className="card border-left-warning shadow h-100 py-2">
-        <div className="card-body">
-            <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                    <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-                    </div>
-                    <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                </div>
-                <div className="col-auto">
-                    <i className="fas fa-user fa-2x text-gray-300"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-{/* <!-- End movies in Data Base --> */}
-</React.Fragment>
-    )
+function ContentRowMovies() {
+  return (
+    <React.Fragment>
+      {/* <!-- Content Row Movies--> */}
+      <div className="row">
+        {cards.map((dataCard, index) => {
+          return <Cards {...dataCard} key={dataCard + index} />;
+        })}
+      </div>
+      {/* <!-- End movies in Data Base --> */}
+    </React.Fragment>
+  );
 }
 
-export default ContentRowMovies
+export default ContentRowMovies;
